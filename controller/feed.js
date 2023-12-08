598cad394c3d7eb5f34c4c623fa84f988f10b7a0
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2023-12-08 12:46:57 +0500

    getting all post endpoint is done a bit

--- controller/feed.js
+++ controller/feed.js
@@ -1,14 +1,18 @@
 exports.getPost = (req , res , next ) =>{
     res.status(200).json({
-        "message" : "This is the message from the get post route",
+        "message" : "This is the message from the get posts route",
         posts : [{
+            _id :'1', // in reality it will be the moongoose id from the database but now adding dummy one
             title : 'This is the title of my first post',
-            content : 'content of first post'
-        } , 
-        {
-            title : 'second title',
-            content : 'content of the second post'
-        }]
+            content : 'content of first post',
+            imageUrl : 'images/coffee.jpg',
+            creator : {
+                name : 'Abdullah Bin Arshad'
+            },
+            createdAt : new Date()
+            
+        } 
+      ]
     })
 }
 
