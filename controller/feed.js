b80a9e7f177d392dddd32b999faf61921c31e083
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2023-12-09 22:51:30 +0500

    getting and posting routes are done bit , bit of faked data of user etc

--- controller/feed.js
+++ controller/feed.js
@@ -28,7 +28,11 @@ exports.createPost = (req, res , next) =>{
         post : {
             id : new Date().toISOString(),
             title : title ,
-            content : content
+            content : content,
+            creator : {
+                name : 'Abdullah bin Arshad'
+            },
+            createdAt : new Date()
         }
 
     })
