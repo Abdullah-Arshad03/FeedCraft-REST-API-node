dc0b59cea7d58e311cae53f51070bfe73489ce8b
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2023-12-12 19:19:57 +0500

    updating post is now working

--- controller/feed.js
+++ controller/feed.js
@@ -1,6 +1,9 @@
 const {validationResult} = require('express-validator')
+const fs = require('fs')
+const path = require('path')
 
 const Post = require('../models/post')
+const { ValidationHalt } = require('express-validator/src/base')
 
 
 exports.getPosts = (req , res , next ) =>{
@@ -94,7 +97,49 @@ exports.getPost = (req,res,next)=>{
 
 exports.editPost = () =>{
     const postId = req.params.postId
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        const error = new Error(errors.array()[0].msg)
+        error.statusCode = 422
+        throw error
+    }
     const title = req.body.title
     const content = req.body.content
     let imageUrl = req.body.image
+
+    if(req.file){
+        imageUrl = req.file.path
+    }
+    if(!imageUrl){
+    const error = new Error('no image is provided !')
+    error.statusCode = 422
+    throw error
+    }
+    Post.findById(postId).then((post)=>{
+        if (!post){
+            const error = new Error('post isnt found in the database')
+            error.statusCode = 404
+            throw error
+        }
+        if(imageUrl != post.imageUrl){
+            clearImage(post.imageUrl)
+        }
+        post.title = title
+        post.content = content
+        post.imageUrl = imageUrl
+        return post.save()
+    }).then((result)=>{
+        res.status(200).json({message : 'post is updated successfully !', post : result})
+    }).catch(err =>{
+        next(err)
+    })
+
+}
+
+//creating a helper function now 
+
+const clearImage = (filepath) =>{
+     filepath = path.join(__dirname , '..' , filepath)
+     fs.unlink(filePath , err =>{console.log(err)})
 }
