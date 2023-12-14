e1d077766740b1f5c0428b0e22fedb3eea526104
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2023-12-15 02:23:50 +0500

    working on connection posts with the users

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,7 +1,13 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res , next) =>{
-   const token = req.get('Authorization').split(' ')[1]
+    const authHeader = req.get('Authorization')
+    if(!authHeader){
+         const error = new Error('not Authenticated!')
+         error.statusCode = 401
+         throw error
+    }
+   const token = authHeader.split(' ')[1]
    console.log(token)
    // now we decode this token received from the frontend
    let decodedToken;
@@ -17,8 +23,6 @@ module.exports = (req, res , next) =>{
     error.statusCode = 401
     throw error
    }
-
    req.user = decodedToken.userId 
    next()
-
 }
--- routes/feed.js
+++ routes/feed.js
@@ -1,26 +1,27 @@
 const express = require('express')
 
 const { check } = require('express-validator')
+const isAuth = require('../middleware/is-auth')
 
 const router = express.Router()
 const feedController = require('../controller/feed')
 
-router.get('/posts', feedController.getPosts)
+router.get('/posts',isAuth ,  feedController.getPosts)
 
 router.post('/post',[
     check('title','title must contain atleast 5 characters!').trim().isLength({min : 5}),
     check('content', 'content must contain atleast 5 characters!').trim().isLength({min : 5})
-], feedController.createPost)
+], isAuth ,feedController.createPost)
 
-router.get('/post/:postId' , feedController.getPost)
+router.get('/post/:postId' , isAuth ,feedController.getPost)
 
 // this route is for editing the post
 router.put('/post/:postId', [
     check('title','title must contain atleast 5 characters!').trim().isLength({min : 5}),
     check('content', 'content must contain atleast 5 characters!').trim().isLength({min : 5})
-] , feedController.editPost)
+] ,isAuth , feedController.editPost)
 
-router.delete('/post/:postId', feedController.deletePost)
+router.delete('/post/:postId',isAuth, feedController.deletePost)
 
 
 module.exports = router
