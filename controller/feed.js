a6375633f779803095a33e2aea620086aafed58d
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2023-12-27 13:03:24 +0500

    done some changes i-e removed pagination for feedcraft at the moment

--- controller/feed.js
+++ controller/feed.js
@@ -10,16 +10,10 @@ exports.getPosts = (req, res, next) => {
   const perPage = 2;
   // same value as given in the frontend, we can setup this in such way that it goes from the backend and then we do all but no worries right now hardcoding it putting the same value of 2 and we want on the frontend
 
+
+ 
   let totalItems;
 
-  // .countDocuments()
-  // .then((count) => {
-  //   totalItems = count;
-  //   // here below we cannot not only find the items but also perform pagination
-  //   return Post.find()
-  //     .skip((currentPage - 1) * perPage)
-  //     .limit(perPage);
-  // })
   Post.find().then((posts) => {
       if (!posts) {
         const error = new Error("Posts arent fetched from the DB");
@@ -201,7 +195,7 @@ exports.deletePost = (req, res, next) => {
         return user.save()
         
       }).then((result)=>{
-        res.json({ message: "post is deleted! successfully !" });
+        res.status(200).json({ message: "post is deleted! successfully !" });
       });
     })
     .catch((err) => {
