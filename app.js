9a77c75ef9bb9f36955a22775ac24e55562da133
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-01-26 04:12:14 +0500

    almost done with all sort of things

--- app.js
+++ app.js
@@ -64,7 +64,16 @@ app.use((error,req,res,next)=>{
 
 mongoose.connect('mongodb://127.0.0.1:27017/blogDb').then((connected)=>{
     console.log('Mongoose Connected!')
-    app.listen(8080)
+    
+    const server = app.listen(8080)
+    const {Server} = require ('socket.io')
+    const io =  new Server(server, {
+        cors: {
+            origin: ['http://localhost:3000', 'http://localhost:3001'],
+            methods: ['GET', 'POST' , 'PUT'],
+            credentials: true,
+          }
+    })
 }).catch((err)=>{
     console.log(err)
 })
--- controller/profile.js
+++ controller/profile.js
@@ -4,14 +4,14 @@ const Post = require('../models/post')
 exports.getProfile = async (req, res, next)=>{
     
 try{
-   const posts = await Post.find({creator : req.user})
+   const posts = await Post.find({creator : req.user}).populate('creator')
 
    if(!posts){
     const error = new Error('The User isnt Authorized!')
     error.statusCode = 403 // code for the forbiden, means resource isnt belong to the user
     throw error
    }
-   res.json({
+   res.status(200).json({
     message :'the logged in users posts are here',
     posts : posts
    })
